a6af6f93d781f6da96f9c160790a9b1fa81f0658
wwf <790107894@qq.com>
2020-04-10 11:42:58 +0800

    添加注释

--- src/main/java/com/wei/demo/attribute/ChannelAttribute.java
+++ src/main/java/com/wei/demo/attribute/ChannelAttribute.java
@@ -3,6 +3,9 @@ package com.wei.demo.attribute;
 import com.wei.demo.session.Session;
 import io.netty.util.AttributeKey;
 
+/**
+ * Channel属性
+ */
 public interface ChannelAttribute {
 
     AttributeKey<Session> SESSION = AttributeKey.newInstance("session");
